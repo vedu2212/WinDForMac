024bdb3aec59416b7b600a960435df7a22d50a12
Vedant Vyas <vedu2212@gmail.com>
2026-03-23 16:57:02 -0400

    Initial Commit

---
+++ WinDForMac/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WinDForMac
+//
+//  Created by Vedant Vyas on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WinDForMac/WinDForMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  WinDForMacApp.swift
+//  WinDForMac
+//
+//  Created by Vedant Vyas on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct WinDForMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
